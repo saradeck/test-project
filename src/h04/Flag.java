88c7486bad4fa32687ed89b16cf4e0fa2861216a
simon <sdegroot@roc-dev.com>
2016-09-09 11:16:55 +0200

    git project

--- src/h04/Flag.java
+++ src/h04/Flag.java
@@ -1,19 +1,46 @@
 package h04;
 
-        import java.awt.*;
-        import java.applet.*;
+import java.applet.*;
+
+import java.awt.*;
+
+
+/*<applet code='flag.java' height=500 width=800></applet>*/
 
 public class Flag extends Applet {
 
-    public void init() {
+    public void init()
+    {
     }
-
     public void paint(Graphics g) {
-        g.drawRect(200, 200, 200, 200);
-        g.drawRect(250, 250, 40, 40);
-        g.drawRect(300, 300, 40, 100);
-        g.drawLine(200, 200, 300, 100);
-        g.drawLine(400, 200, 300, 100);
-    }
 
+
+
+
+
+
+        Color c1 = new Color(230, 39, 30);
+
+
+
+        g.setColor(Color.black);
+
+        g.fillRect(500, 100, 5, 400);
+
+        g.setColor(c1);
+
+        g.fillRect(505, 102, 180, 40);
+
+        g.setColor(Color.white);
+
+        g.fillRect(505, 142, 180, 40);
+
+        g.setColor(Color.blue);
+
+        g.fillRect(505, 182, 180, 40);
+
+
+
+
+    }
 }

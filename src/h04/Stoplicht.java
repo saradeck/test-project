3d01998432640743fbceeba115b4d13efe3bef9b
simon <sdegroot@roc-dev.com>
2016-09-13 08:56:31 +0200

    git project

--- src/h04/Stoplicht.java
+++ src/h04/Stoplicht.java
@@ -9,12 +9,12 @@ public class Stoplicht extends Applet
 
     public void paint( Graphics g )
     {
-        g.setColor(Color.gray);
+        g.setColor(Color.black);
         g.fillRect(200,100,50,150);
         g.fillRect(220,250,10,300);
         g.setColor(Color.red);
         g.fillOval(200,100,50,50);
-        g.setColor(Color.blue);;
+        g.setColor(Color.yellow);;
         g.fillOval(200,150,50,50);
         g.setColor(Color.green);
         g.fillOval(200,200,50,50);

ff0279ba5a38ed976b2c0932cd89f02ff4851ea0
simon <sdegroot@roc-dev.com>
2016-11-25 11:42:54 +0100

    git project

--- src/H13/Bomen.java
+++ src/H13/Bomen.java
@@ -3,5 +3,36 @@ package H13;
 /**
  * Created by simon on 25-11-2016.
  */
-public class Bomen {
-}
+import java.awt.*;
+import java.applet.*;
+
+
+
+public class Bomen extends Applet {
+    int x = 100;
+
+
+    public void init() {
+        setSize(1200,200);
+    }
+
+    public void tekenboom( Graphics g, int x1, int y1, int x2, int y2) {
+        g.setColor(Color.black);
+        g.fillRect(x1, y1, 20, 60);
+        g.setColor(Color.GREEN);
+        g.fillOval(x2, y2, 60, 60);
+    }
+    public void paint(Graphics g) {
+        for (int teller = 0; teller < 10; teller++) {
+            x += 100;
+            
+
+
+            tekenboom(g,x,120,x - 20,60);
+        }
+    }
+
+
+
+        }
+
--- src/H13/TweeMuren.java
+++ src/H13/TweeMuren.java
@@ -3,5 +3,84 @@ package H13;
 /**
  * Created by simon on 25-11-2016.
  */
-public class TweeMuren {
+import java.applet.Applet;
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TweeMuren extends Applet {
+
+    private Button baksteenknop;
+    private Button betonknop;
+    int breedtesteen;
+    int hoogtesteen;
+    private int AantalSteenHoogte;
+    private int AantalSteenbreedte;
+    Color kleur1;
+    Color kleur2;
+    boolean muurteken;
+
+    public void init(){
+        setSize(700, 400);
+        baksteenknop = new Button("Bakstenen");
+        baksteenknop.addActionListener(new BaksteenLuisteraar());
+
+
+        betonknop = new Button("Betonblokken");
+        betonknop.addActionListener(new BetonblokLuisteraar());
+
+
+        muurteken = false;
+        kleur2 = Color.BLACK;
+        repaint();
+        add(baksteenknop);
+        add(betonknop);
+
+    }
+
+    public void paint(Graphics g){
+        int x = 50;
+        int y = 50;
+        if (muurteken){
+            for (int i = 0; i < AantalSteenbreedte; i++){
+                for( int j = 0; j < AantalSteenHoogte; j++){
+                    g.setColor(kleur1);
+                    g.fillRect(x, y, breedtesteen, hoogtesteen);
+                    g.setColor(kleur2);
+                    g.drawRect(x, y, breedtesteen, hoogtesteen);
+                    x += breedtesteen;
+                }
+                y += hoogtesteen;
+                x = 50;
+            }
+
+        }
+    }
+
+    class BaksteenLuisteraar implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            breedtesteen = 20;
+            hoogtesteen = 15;
+            AantalSteenHoogte = 25;
+            AantalSteenbreedte = 20;
+            kleur1 = Color.RED;
+            muurteken = true;
+            repaint();
+        }
+
+    }
+    class BetonblokLuisteraar implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            breedtesteen = 40;
+            hoogtesteen = 30;
+            AantalSteenHoogte = 15;
+            AantalSteenbreedte = 10;
+            kleur1 = Color.GRAY;
+            muurteken = true;
+            repaint();
+        }
+
+    }
 }
